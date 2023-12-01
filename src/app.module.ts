aa2e84fb4a92f61b587206bfd38359e6375f6a48
Kirill Naumenko <naumenkokirill466@gmail.com>
2023-12-01 05:15:15 +0300

    feat: ✨ Add mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppConfigService } from './env.interface';
 
 @Module({
-  imports: [ConfigModule.forRoot({ ignoreEnvFile: true })],
+  imports: [
+    ConfigModule.forRoot({ ignoreEnvFile: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: AppConfigService) => ({
+        uri: configService.get('MONGO_CONNECTION_STRING'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
