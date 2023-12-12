d7f15bee2782517c0ac8e472c6f8e7660d052185
Kirill Naumenko <naumenkokirill466@gmail.com>
2023-12-12 18:16:02 +0300

    feat: ✨ Add new env variables types

--- src/env.interface.ts
+++ src/env.interface.ts
@@ -1,8 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 
 export interface Env {
+  COOKIE_SECRET: string;
+  JWT_ACCESS_EXP_TIME: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_REFRESH_EXP_TIME: string;
   MONGO_CONNECTION_STRING: string;
   NODE_ENV: 'development' | 'production' | 'test';
+  PASSWORD_HASH_ROUNDS: string;
+  REDIS_CONNECTION_STRING: string;
 }
 
 export type AppConfigService = ConfigService<Env>;
