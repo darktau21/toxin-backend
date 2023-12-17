9a9271c2131f0cb2a20bca506a6c9e84bccfc7e9
Kirill Naumenko <naumenkokirill466@gmail.com>
2023-12-18 01:44:09 +0300

    refactor: 💅 Move jest config to a separate file

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import { type Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts',
+  ],
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/$1",
+  },
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
