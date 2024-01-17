44708305584b0411e3b332770d571e7d0c613401
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-01-17 11:23:14 +0300

    refactor: 💅 Refactor cache module

--- src/app/modules/cache.module.ts
+++ src/app/modules/cache.module.ts
@@ -1,13 +1,13 @@
 import { CacheModule } from '@nestjs/cache-manager';
-import { RedisService } from '@songkeys/nestjs-redis';
+import { RedisModule, RedisService } from '@songkeys/nestjs-redis';
+import { Config } from 'cache-manager';
 import { redisInsStore } from 'cache-manager-ioredis-yet';
 
 import { REDIS_CACHE } from '~/app/modules/redis.module';
 import { AppConfigService } from '~/config/app-config.service';
 
-export const cacheModule = CacheModule.registerAsync({
+export const cacheModule = CacheModule.registerAsync<Config>({
   inject: [RedisService, AppConfigService],
-  isGlobal: true,
   useFactory: async (
     redisService: RedisService,
     configService: AppConfigService,
