11ec5654f531b4a79710c66211307c92479d3551
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-01-26 04:57:45 +0300

    refactor: 💅 Small style changes

--- src/auth/decorators/public.decorator.ts
+++ src/auth/decorators/public.decorator.ts
@@ -4,10 +4,8 @@ import { Reflector } from '@nestjs/core';
 export const PUBLIC_KEY = 'public';
 export const Public = () => SetMetadata(PUBLIC_KEY, true);
 
-export const isPublic = (ctx: ExecutionContext, reflector: Reflector) => {
-  const isPublic = reflector.getAllAndOverride<boolean>(PUBLIC_KEY, [
+export const isPublic = (ctx: ExecutionContext, reflector: Reflector) =>
+  reflector.getAllAndOverride<boolean>(PUBLIC_KEY, [
     ctx.getHandler(),
     ctx.getClass(),
   ]);
-  return isPublic;
-};
