30758ab1d43a56f14128b83cf446b766f8d22a61
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-01-31 20:40:37 +0300

    refactor: 💅 Remove redunant function

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,7 +1,6 @@
 export * from './create-filter-query';
 export * from './clear-object';
 export * from './paginate';
-export * from './apply-filter';
 export * from './async-random-int';
 export * from './async-random-bytes';
 export * from './transform-deleted';
