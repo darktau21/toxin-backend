384dad9a613592916c0e5095b5d026e98b365ee9
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-02-03 20:26:44 +0300

    refactor: 💅 Clear object filters NaN and null values

--- src/app/utils/clear-object.ts
+++ src/app/utils/clear-object.ts
@@ -2,7 +2,13 @@ export function clearObject<T extends Record<string, unknown>>(obj: T) {
   const copy = { ...obj };
 
   Object.entries(copy).forEach(([key, value]) => {
-    if (typeof value === 'undefined') delete copy[key];
+    if (
+      typeof value === 'undefined' ||
+      Object.is(value, NaN) ||
+      Object.is(value, null)
+    ) {
+      delete copy[key];
+    }
   });
 
   return copy;
