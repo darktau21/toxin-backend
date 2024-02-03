5d630b44ffcc5ab43355b63f50960138a339c6e3
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-02-03 19:40:25 +0300

    feat: ✨ Add verify decorator
    Decorator checks if user is verified or not

--- src/auth/decorators/verify.decorator.ts
+++ src/auth/decorators/verify.decorator.ts
@@ -3,7 +3,10 @@ import { Reflector } from '@nestjs/core';
 
 export const Verify = Reflector.createDecorator({ transform: () => true });
 
-export const isVerified = (context: ExecutionContext, reflector: Reflector) =>
+export const isVerificationRequired = (
+  context: ExecutionContext,
+  reflector: Reflector,
+) =>
   reflector.getAllAndOverride(Verify, [
     context.getHandler(),
     context.getClass(),
--- src/auth/guards/verify.guard.ts
+++ src/auth/guards/verify.guard.ts
@@ -2,24 +2,18 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { FastifyRequest } from 'fastify';
 
-import { UserService } from '~/user/user.service';
-
-import { isVerified as isVerificationRequired } from '../decorators';
+import { isVerificationRequired } from '../decorators';
 
 @Injectable()
 export class VerifyGuard implements CanActivate {
-  constructor(
-    private readonly reflector: Reflector,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext) {
     if (!isVerificationRequired(context, this.reflector)) {
       return true;
     }
-    const userId = context.switchToHttp().getRequest<FastifyRequest>().user?.id;
+    const { user } = context.switchToHttp().getRequest<FastifyRequest>();
 
-    const user = await this.userService.findById(userId);
     return user?.isVerified;
   }
 }
